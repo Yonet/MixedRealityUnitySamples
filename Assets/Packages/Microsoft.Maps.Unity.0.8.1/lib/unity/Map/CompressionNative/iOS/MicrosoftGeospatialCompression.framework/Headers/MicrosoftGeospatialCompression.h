ccba42a1812eae78d3202a10f7d09e7b0a6a29b6
Yonet <aysegulyonet@gmail.com>
2020-06-05 23:11:53 -0700

    Update(mrtk): v 2.4.0

---
+++ Assets/Packages/Microsoft.Maps.Unity.0.8.1/lib/unity/Map/CompressionNative/iOS/MicrosoftGeospatialCompression.framework/Headers/MicrosoftGeospatialCompression.h
@@ -0,0 +1,19 @@
+//
+//  MicrosoftGeospatialCompression.h
+//  MicrosoftGeospatialCompression
+//
+//  Created by Admin on 2/4/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MicrosoftGeospatialCompression.
+FOUNDATION_EXPORT double MicrosoftGeospatialCompressionVersionNumber;
+
+//! Project version string for MicrosoftGeospatialCompression.
+FOUNDATION_EXPORT const unsigned char MicrosoftGeospatialCompressionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MicrosoftGeospatialCompression/PublicHeader.h>
+
+
